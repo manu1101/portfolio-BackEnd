8f8b5aa44703cb26637f10bd349c0f186eda4608
manu1101 <manu_1101@hotmail.com>
2022-07-26 22:50:31 -0300

    Cors

--- src/main/java/com/jmc/portfolioBack/PortfolioBackApplication.java
+++ src/main/java/com/jmc/portfolioBack/PortfolioBackApplication.java
@@ -2,12 +2,27 @@ package com.jmc.portfolioBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioBackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioBackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioBackApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
