e02f5dcb245a70e5b4b7797f9e79de68016ccb03
manu1101 <manu_1101@hotmail.com>
2022-07-26 22:32:34 -0300

    Cors

--- src/main/java/com/jmc/portfolioBack/controller/Controller.java
+++ src/main/java/com/jmc/portfolioBack/controller/Controller.java
@@ -5,14 +5,17 @@ import com.jmc.portfolioBack.model.Persona;
 import com.jmc.portfolioBack.service.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class Controller {
     
     @Autowired
